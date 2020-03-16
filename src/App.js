66473b06ea8350183d74af97bd6cc3d485893e6c
Mekhala Joshi <mekhalajoshi@gmail.com>
2020-03-16 16:00:28 -0400

    content update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Travis CI</h1>
+				<p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+				</a>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
