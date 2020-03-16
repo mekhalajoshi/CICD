f9233d23b2e51e2b72777b7b642bb169b18dcab1
Mekhala Joshi <mekhalajoshi@gmail.com>
2020-03-16 16:16:46 -0400

    Content update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>Travis CI</h1>
+				<h1>Travis CI!!</h1>
 				<p>
           Edit <code>src/App.js</code> and save to reload.
 				</p>
