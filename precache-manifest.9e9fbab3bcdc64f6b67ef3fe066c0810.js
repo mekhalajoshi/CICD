4704c6458da935229e3eea2c8cc7ce7de2df6cc0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-16 20:27:51 +0000

    Deploy mekhalajoshi/CICD to github.com/mekhalajoshi/CICD.git:gh-pages

---
+++ precache-manifest.9e9fbab3bcdc64f6b67ef3fe066c0810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f6111bf47dc2e89ebe5b8fbba3255d",
+    "url": "/CICD/index.html"
+  },
+  {
+    "revision": "6c802478ac5e8b1b9c53",
+    "url": "/CICD/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "23ee2a3dc0f8f4923a89",
+    "url": "/CICD/static/js/2.8e490249.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/CICD/static/js/2.8e490249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c802478ac5e8b1b9c53",
+    "url": "/CICD/static/js/main.094844f5.chunk.js"
+  },
+  {
+    "revision": "7fa2133be72d61a50d99",
+    "url": "/CICD/static/js/runtime-main.e376c21e.js"
+  }
+]);
